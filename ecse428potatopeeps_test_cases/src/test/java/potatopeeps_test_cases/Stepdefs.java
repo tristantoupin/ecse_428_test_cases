5566f3a6c4e6d1fac08aa1d606fffa21fcbbbc35
ismailfaruk <ismailfaruk.17@gmail.com>
2019-03-28 19:42:07 -0400

    login stepdef added

--- ecse428potatopeeps_test_cases/src/test/java/potatopeeps_test_cases/Stepdefs.java
+++ ecse428potatopeeps_test_cases/src/test/java/potatopeeps_test_cases/Stepdefs.java
@@ -6,9 +6,10 @@ import cucumber.api.java.en.When;
 
 import static org.junit.Assert.*;
 
-public class Stepdefs {
 
-    @Given("Alice has installed maven")
+public class Stepdefs {
+/*
+    @Given("That I am logged in as a custumer")
     public void alice_has_installed_maven() {
         // Write code here that turns the phrase above into concrete actions
     }
@@ -22,5 +23,32 @@ public class Stepdefs {
     @Then("maven will setup a new cucumber project")
     public void maven_will_setup_a_new_cucumber_project() {
         // Write code here that turns the phrase above into concrete actions
+    }*/
+    
+    @Given("that I am logged in as a custumer")
+    public void that_I_am_logged_in_as_a_custumer() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
     }
+    
+    @When("I select a category to browse")
+    public void i_select_a_category_to_browse() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+    
+    @When("selects an item of the menu")
+    public void selects_an_item_of_the_menu() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+    
+    @Then("I see a description of the item")
+    public void i_see_a_description_of_the_item() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+    
+    
 }
+
--- ecse428potatopeeps_test_cases/target/test-classes/potatopeeps_test_cases/cucumber.feature
+++ ecse428potatopeeps_test_cases/target/test-classes/potatopeeps_test_cases/cucumber.feature
@@ -1,8 +1,31 @@
-Feature: Setting up a maven archetype
 
-  To quickly start a new project a maven archetype is usefull
+#----------------- Default Structure----------------
+#Feature: Setting up a maven archetype
 
-  Scenario:
-    Given Alice has installed maven
-    When and she runs the maven command from the read me
-    Then maven will setup a new cucumber project
+#  To quickly start a new project a maven archetype is usefull
+
+#  Scenario:
+#    Given Alice has installed maven
+#    When and she runs the maven command from the read me
+#    Then maven will setup a new cucumber project
+
+#----------------- Default Structure----------------
+
+# Story 1
+Feature: Browse item in the menu
+    # Normal flow
+    Scenario: The customer successfully browses the menu
+        Given that I am logged in as a custumer
+        When I select a category to browse
+        And selects an item of the menu
+        Then I see a description of the item
+
+# Story 2 and 5
+Feature: Create an order
+    # Normal flow
+    Scenario: The customer successfully creates an order
+        Given that I am logged in as a custumer and browsing a categorie of items
+        When I select an item from the menu to add to my card
+        And navigate to the cart
+        And submit the order
+        Then the staff receives a new oder
