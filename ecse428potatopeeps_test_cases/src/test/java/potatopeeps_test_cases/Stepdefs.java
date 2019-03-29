3d96662eb8954ee00ad0f4444c53a165a952647d
ismailfaruk <ismailfaruk.17@gmail.com>
2019-03-28 20:31:41 -0400

    step def skeleton added, less examples

--- ecse428potatopeeps_test_cases/src/test/java/potatopeeps_test_cases/Stepdefs.java
+++ ecse428potatopeeps_test_cases/src/test/java/potatopeeps_test_cases/Stepdefs.java
@@ -8,47 +8,265 @@ import static org.junit.Assert.*;
 
 
 public class Stepdefs {
-/*
-    @Given("That I am logged in as a custumer")
-    public void alice_has_installed_maven() {
-        // Write code here that turns the phrase above into concrete actions
-    }
 
-    @When("and she runs the maven command from the read me")
-    public void and_she_runs_the_maven_command_from_the_read_me() {
-        // Write code here that turns the phrase above into concrete actions
-    }
-
-
-    @Then("maven will setup a new cucumber project")
-    public void maven_will_setup_a_new_cucumber_project() {
-        // Write code here that turns the phrase above into concrete actions
-    }*/
-    
     @Given("that I am logged in as a custumer")
     public void that_I_am_logged_in_as_a_custumer() {
         // Write code here that turns the phrase above into concrete actions
         throw new cucumber.api.PendingException();
     }
-    
+
     @When("I select a category to browse")
     public void i_select_a_category_to_browse() {
         // Write code here that turns the phrase above into concrete actions
         throw new cucumber.api.PendingException();
     }
-    
+
     @When("selects an item of the menu")
     public void selects_an_item_of_the_menu() {
         // Write code here that turns the phrase above into concrete actions
         throw new cucumber.api.PendingException();
     }
-    
+
     @Then("I see a description of the item")
     public void i_see_a_description_of_the_item() {
         // Write code here that turns the phrase above into concrete actions
         throw new cucumber.api.PendingException();
     }
-    
-    
+
+    @Given("that I am logged in as a custumer and browsing a categorie of items")
+    public void that_I_am_logged_in_as_a_custumer_and_browsing_a_categorie_of_items() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I select an item from the menu to add to my card")
+    public void i_select_an_item_from_the_menu_to_add_to_my_card() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("navigate to the cart")
+    public void navigate_to_the_cart() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("submit the order")
+    public void submit_the_order() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the staff receives a new oder")
+    public void the_staff_receives_a_new_oder() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I navigate to the bill")
+    public void i_navigate_to_the_bill() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("and submit a request to the staff to pay the bill")
+    public void and_submit_a_request_to_the_staff_to_pay_the_bill() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("the staff receives a new bill request")
+    public void the_staff_receives_a_new_bill_request() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the staff clears the bill request")
+    public void the_staff_clears_the_bill_request() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I request the staff for help")
+    public void i_request_the_staff_for_help() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("the staff receives a new help request")
+    public void the_staff_receives_a_new_help_request() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the staff clears the request")
+    public void the_staff_clears_the_request() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("that I am logged in as a staff")
+    public void that_I_am_logged_in_as_a_staff() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I navigate the orders")
+    public void i_navigate_the_orders() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("the customer submits and order")
+    public void the_customer_submits_and_order() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("changes the status of the order to <status>")
+    public void changes_the_status_of_the_order_to_status() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the status persists")
+    public void the_status_persists() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("that I am logged in as a manager")
+    public void that_I_am_logged_in_as_a_manager() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I enter the <price> of an item")
+    public void i_enter_the_price_of_an_item() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I enter the <name> of an item")
+    public void i_enter_the_name_of_an_item() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I enter the <description> of an item")
+    public void i_enter_the_description_of_an_item() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I enter the <inventory> of an item")
+    public void i_enter_the_inventory_of_an_item() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I enter the <tag> of an item")
+    public void i_enter_the_tag_of_an_item() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("submits the item")
+    public void submits_the_item() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the item persists")
+    public void the_item_persists() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("delete an item")
+    public void delete_an_item() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("the system prompts me if I am sure that I want to delete the item")
+    public void the_system_prompts_me_if_I_am_sure_that_I_want_to_delete_the_item() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I select that I am sure")
+    public void i_select_that_I_am_sure() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the item is deleted")
+    public void the_item_is_deleted() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("update the <name> of the  an item")
+    public void update_the_name_of_the_an_item() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I save the changes")
+    public void i_save_the_changes() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the item's name was updated")
+    public void the_item_s_name_was_updated() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("update the <price> of the  an item")
+    public void update_the_price_of_the_an_item() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("update the <description> of the  an item")
+    public void update_the_description_of_the_an_item() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("update the <quantity> of the  an item")
+    public void update_the_quantity_of_the_an_item() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("update the <tag> of the  an item")
+    public void update_the_tag_of_the_an_item() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("that I am logged in")
+    public void that_I_am_logged_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I select the task <task>")
+    public void i_select_the_task_task() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the landing page of that task is open")
+    public void the_landing_page_of_that_task_is_open() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
 }
 
--- ecse428potatopeeps_test_cases/target/test-classes/potatopeeps_test_cases/cucumber.feature
+++ ecse428potatopeeps_test_cases/target/test-classes/potatopeeps_test_cases/cucumber.feature
@@ -11,6 +11,7 @@
 
 #----------------- Default Structure----------------
 
+
 # Story 1
 Feature: Browse item in the menu
     # Normal flow
@@ -21,11 +22,107 @@ Feature: Browse item in the menu
         Then I see a description of the item
 
 # Story 2 and 5
-Feature: Create an order
-    # Normal flow
+@tag Feature: Create an order
     Scenario: The customer successfully creates an order
         Given that I am logged in as a custumer and browsing a categorie of items
         When I select an item from the menu to add to my card
         And navigate to the cart
         And submit the order
         Then the staff receives a new oder
+
+
+# Story 3 and 8
+@tag Feature: Request the bill
+    # Normal flow
+    Scenario: The customer successfully review and request the bill
+        Given that I am logged in as a custumer
+        When I navigate to the bill
+        And and submit a request to the staff to pay the bill
+        And the staff receives a new bill request
+        Then the staff clears the bill request
+# Story 4 and 7
+@tag Feature: Query the staff
+    # Normal flow
+    Scenario: The customer successfully queries the staff
+        Given that I am logged in as a custumer
+        When I request the staff for help
+        And the staff receives a new help request
+        Then the staff clears the request
+        
+# Story 6
+@tag Feature: Change status of an order
+    # Normal flow
+    Scenario: The staff successfully changes the status of an order
+        Given that I am logged in as a staff
+        When I navigate the orders
+        And the customer submits and order
+        And changes the status of the order to <status>
+        Then the status persists
+
+# Story 9
+@tag Feature: Add item to the menu
+    # Normal flow
+    Scenario: The manager successfully adds an item to the menu
+        Given that I am logged in as a manager
+        When I enter the <price> of an item
+        And I enter the <name> of an item
+        And I enter the <description> of an item
+        And I enter the <inventory> of an item
+        And I enter the <tag> of an item
+        And submits the item
+        Then the item persists
+
+
+# Story 10
+@tag Feature: Delete item from the menu
+    # Normal flow
+    Scenario: The manager successfully deletes an item to the menu
+        Given that I am logged in as a manager
+        When delete an item
+        And the system prompts me if I am sure that I want to delete the item
+        And I select that I am sure
+        Then the item is deleted
+# Story 11
+@tag Feature: Update item from the menu
+    # Normal flow
+    Scenario: The manager successfully updates the name of an item to the menu
+        Given that I am logged in as a manager
+        When update the <name> of the  an item
+        And I save the changes
+        Then the item's name was updated
+
+    # Alternate flow
+    Scenario: The manager successfully updates the price of an item to the menu
+        Given that I am logged in as a manager
+        When update the <price> of the  an item
+        And I save the changes
+        Then the item's name was updated
+
+    # Alternate flow
+    Scenario: The manager successfully updates the description of an item to the menu
+        Given that I am logged in as a manager
+        When update the <description> of the  an item
+        And I save the changes
+        Then the item's name was updated
+
+    # Alternate flow
+    Scenario: The manager successfully updates the quantity of an item to the menu
+        Given that I am logged in as a manager
+        When update the <quantity> of the  an item
+        And I save the changes
+        Then the item's name was updated
+
+    # Alternate flow
+    Scenario: The manager successfully updates the tag of an item to the menu
+        Given that I am logged in as a manager
+        When update the <tag> of the  an item
+        And I save the changes
+        Then the item's name was updated
+
+# Story 12
+@tag Feature: Update item from the menu
+    # Normal flow
+    Scenario: The manager successfully updates the name of an item to the menu
+        Given that I am logged in
+        When I select the task <task>
+        Then the landing page of that task is open
